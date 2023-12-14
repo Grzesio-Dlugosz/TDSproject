3cc7e20316769a9858acd61f6cb4abb648537bbf
Grzesio-Dlugosz <112557416+Grzesio-Dlugosz@users.noreply.github.com>
2023-12-14 17:42:44 +0100

    Added few files to gitignore Renaming Changed copyright notice Added .idea/gitignore

--- Source/TDSproject/TDSprojectGameMode.cpp
+++ Source/TDSproject/TDSprojectGameMode.cpp
@@ -1,14 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "TDSprojectGameMode.h"
-#include "TDSprojectPlayerController.h"
-#include "TDSprojectCharacter.h"
+#include "TopDownPlayerController.h"
+//#include "Characters/Player/PlayerCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATDSprojectGameMode::ATDSprojectGameMode()
 {
 	// use our custom PlayerController class
-	PlayerControllerClass = ATDSprojectPlayerController::StaticClass();
+	PlayerControllerClass = ATopDownPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
